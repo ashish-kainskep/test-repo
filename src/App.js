199f214bc0503c68c8f34b5160ed23090a9a48a0
Ashish Pandey <ashish.pandey@kainskep.com>
2024-05-22 11:12:32 +0530

    beanstalk

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Hello World!
+          Hello World by AK47!
         </p>
       </header>
     </div>
