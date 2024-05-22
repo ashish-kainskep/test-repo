496752b6da9f47af8b0c053ecc3fd19150c006c1
Ashish Pandey <ashish.pandey@kainskep.com>
2024-05-22 11:14:47 +0530

    pipline

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Hello World by AK47!
+          Hello World for pipeline test!
         </p>
       </header>
     </div>
